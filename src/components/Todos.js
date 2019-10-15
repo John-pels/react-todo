5721f303868d9497bc95c9510706b9379713b8f1
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2019-10-15 05:41:51 +0100

    I create the Todos component

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
 
-
-function Todos() {
-
-    return ( 
-    <div>
-        <h1 > Todos </h1>  
-         </div>
-    );
+ class Todos extends Component {
+    render() {
+    return this.props.todos.map((todo) =>(
+   <TodoItem key={todo.id} todo={todo} />
+    ));
 
 
 }
+}
 
+//Proptypes
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired
+}
 export default Todos;
