dd24dd0691ba80d3278e273b10bad29747725ec7
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2019-10-17 04:38:10 +0100

    Updates

---
+++ precache-manifest.3d5b30e1a136431cd5897478fe59a751.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e43fc6ff3ca58793bd6120a422c5e47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49facfa4fc9a29e37e18",
+    "url": "/static/css/main.8b662860.chunk.css"
+  },
+  {
+    "revision": "b5c612a0c7f0b38768e9",
+    "url": "/static/js/2.3c1cca94.chunk.js"
+  },
+  {
+    "revision": "49facfa4fc9a29e37e18",
+    "url": "/static/js/main.ec7da15d.chunk.js"
+  },
+  {
+    "revision": "844744758d37a847d2c3",
+    "url": "/static/js/runtime-main.08d34d3a.js"
+  }
+]);
